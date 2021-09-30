222195f985d9d422bdd45c996b473ef510e89ab3
Higon Cesar <higondourado@denox.com.br>
2021-09-30 20:11:46 -0300

    feat(chart bar): chart bar components has been created

---
+++ src/components/Chart/Bar/index.tsx
@@ -0,0 +1,50 @@
+import { Canvas } from './style'
+import ChartJS, { ChartDataset } from 'chart.js/auto'
+import { useEffect, useCallback, useRef, FC } from 'react'
+
+interface ChartBarProps {
+  labels: string[]
+  datasets: ChartDataset<'bar', number[]>[]
+}
+
+const ChartBar: FC<ChartBarProps> = ({ labels, datasets }) => {
+  const chartInstance = useRef<ChartJS<'bar'>>()
+
+  const instanciateChart = useCallback(
+    (chartContainerElement: HTMLCanvasElement | null) => {
+      if (chartContainerElement && !chartInstance.current) {
+        const context = chartContainerElement.getContext('2d')
+        chartInstance.current = new ChartJS(context, {
+          type: 'bar',
+          data: {
+            labels,
+            datasets,
+          },
+          options: {
+            responsive: true,
+            scales: {
+              x: {
+                stacked: true,
+              },
+              y: {
+                stacked: true,
+              },
+            },
+          },
+        })
+      }
+    },
+    [labels, datasets]
+  )
+
+  useEffect(() => {
+    if (chartInstance.current) {
+      chartInstance.current.data.datasets = datasets
+      chartInstance.current.update()
+    }
+  }, [chartInstance.current, datasets])
+
+  return <Canvas ref={(elementRef) => instanciateChart(elementRef)} />
+}
+
+export default ChartBar
---
+++ src/components/Chart/Bar/style.ts
@@ -0,0 +1,6 @@
+import styled from '@emotion/styled'
+
+export const Canvas = styled.canvas`
+  width: 100%;
+  max-height: 100%;
+`
