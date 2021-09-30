c4d510b9dee6e4535f8ac5313cb87204f5117670
Higon Cesar <higondourado@denox.com.br>
2021-09-30 20:14:05 -0300

    feat(theme): theme provider has been created and configured

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
+import { CssBaseline, ThemeProvider } from '@material-ui/core'
 import type { AppProps } from 'next/app'
+import theme from '../theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 export default MyApp
