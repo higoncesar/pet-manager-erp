5510b21a3c35f9e4deed14f165d6cdb52d040c98
Higon Cesar <higondourado@denox.com.br>
2021-09-30 23:56:56 -0300

    feat(chart pie component): chart pie component has been created

---
+++ src/components/Chart/Pie/index.tsx
@@ -0,0 +1,46 @@
+import { Canvas } from './style'
+import ChartJS from 'chart.js/auto'
+import { useEffect, useCallback, useRef, FC } from 'react'
+
+interface ChartPieProps {
+  labels: string[]
+  data: number[]
+  backgroundColor: string[]
+}
+
+const ChartPie: FC<ChartPieProps> = ({ data, backgroundColor, labels }) => {
+  const chartInstance = useRef<ChartJS<'doughnut'>>()
+
+  const instanciateChart = useCallback(
+    (chartContainerElement: HTMLCanvasElement | null) => {
+      if (chartContainerElement && !chartInstance.current) {
+        const context = chartContainerElement.getContext('2d')
+        chartInstance.current = new ChartJS(context, {
+          type: 'doughnut',
+          data: {
+            labels,
+            datasets: [{ data, backgroundColor }],
+          },
+          options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            locale: 'pt-BR',
+          },
+        })
+      }
+    },
+    []
+  )
+
+  useEffect(() => {
+    if (chartInstance.current) {
+      chartInstance.current.data.labels = labels
+      chartInstance.current.data.datasets = [{ data, backgroundColor }]
+      chartInstance.current.update()
+    }
+  }, [chartInstance.current, data, backgroundColor, labels])
+
+  return <Canvas ref={(elementRef) => instanciateChart(elementRef)} />
+}
+
+export default ChartPie
---
+++ src/components/Chart/Pie/style.ts
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled'
+
+export const Canvas = styled.canvas`
+  margin: 0;
+  padding: 0;
+  display: block;
+  max-height: 95%;
+`
