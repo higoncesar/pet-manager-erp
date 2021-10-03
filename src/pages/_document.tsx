839e3d305e3b5fbc93ecb5330b267de9edceac68
Higon Cesar <higondourado@denox.com.br>
2021-10-03 17:01:54 -0300

    chore(fonts): import fonts NaughtyMonster and Titillium Web

---
+++ src/pages/_document.tsx
@@ -0,0 +1,36 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="*"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap"
+            rel="stylesheet"
+          />
+          <style jsx>
+            {`
+              @font-face {
+                font-family: 'NaughtyMonster';
+                src: url('/NaughtyMonster.ttf');
+              }
+            `}
+          </style>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
