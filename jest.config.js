c6f5834a462800d2155bba150ca2666e83137ff2
Higon Cesar <higondourado@denox.com.br>
2021-10-03 21:54:52 -0300

    chore(test environment): config test environment with test library and jest

--- jest.config.js
+++ jest.config.js
@@ -5,27 +5,22 @@ module.exports = {
     '!**/node_modules/**',
   ],
   moduleNameMapper: {
-    '^@/components/(.*)$': '<rootDir>/src/components/$1',
-    /* Handle CSS imports (with CSS modules)
-    https://jestjs.io/docs/webpack#mocking-css-modules */
     '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
-
-    // Handle CSS imports (without CSS modules)
     '^.+\\.(css|sass|scss)$': '<rootDir>/src/__mocks__/styleMock.js',
-
-    /* Handle image imports
-    https://jestjs.io/docs/webpack#handling-static-assets */
     '^.+\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/src/__mocks__/fileMock.js',
+    '^@/components/(.*)$': '<rootDir>/src/components/$1',
+    '^@/services/(.*)$': '<rootDir>/src/services/$1',
+    '^@/config/(.*)$': '<rootDir>/src/config/$1',
+    '^@/assets/(.*)$': '<rootDir>/src/assets/$1',
   },
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/src/.next/'],
   testEnvironment: 'jsdom',
   transform: {
-    /* Use babel-jest to transpile tests with the next/babel preset
-    https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object */
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
   },
   transformIgnorePatterns: [
     '/node_modules/',
     '^.+\\.module\\.(css|sass|scss)$',
   ],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 }
--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const withFonts = require('next-fonts')
 module.exports = withFonts({
+  reactStrictMode: true,
   webpack(config, options) {
     return config
   },
